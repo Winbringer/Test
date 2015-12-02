0cb7a4e9983bf75df440128d6cf8c15a400d2d35
Winbringer <voin1213@yandex.ru>
2015-12-02 23:01:58 +0600

    Les 6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,6 +15,18 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+            int x = numList[0];
+            numList.RemoveAt(0);
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
 
