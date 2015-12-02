1305597ecbcf12090fe2b3468acc8419168e7add
Winbringer <voin1213@yandex.ru>
2015-12-02 23:19:22 +0600

    Les 7

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,17 +16,10 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            int x = numList[0];
-            numList.RemoveAt(0);
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
+            VerticalLine vline = new VerticalLine(8, 13, 5, '+');
+            vline.Drow();
             Console.ReadKey();
         }
 
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+        public VerticalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(y,x, sym);
+                pList.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
